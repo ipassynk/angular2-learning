1e5554f0dc2cd511533b8411063136a99c7a641d
juliap <password>
2015-12-12 16:57:19 -0500

    continue learning

--- src/app/heroRender.ts
+++ src/app/heroRender.ts
@@ -1,15 +1,31 @@
-import {Component, Input} from "angular2/angular2";
+import {Component, Input, CORE_DIRECTIVES} from "angular2/angular2";
 import Hero from "./hero";
-
 
 @Component({
     selector: 'hero-render',
+    styles: [`
+    .hero-red {
+        background-color: red;
+        text-decoration: line-through;
+        text-line-through-color: black;
+    }
+    .hero-green {
+        background-color: #42A948;
+    }
+    .hero {
+        padding: 20px 0;
+    }
+    `],
     template: `
-        <div>
+        <div [ng-class]="getHeroClass()" class="hero">
         {{ hero.name }} | {{ hero.power }}
         </div>
     `
 })
 export default class HeroRender {
-    @Input()  hero : Hero;
+    @Input()  hero:Hero;
+
+    getHeroClass() {
+        return this.hero.name === 'Julia' ? "hero-red" : "hero-green";
+    }
 }
