a290850b8609794c0c1a901fe47c20cd871d09b6
Julia.Passynkova <julia.passynkova@nurun.com>
2015-12-15 23:59:03 -0500

    list item

---
+++ src/app/ItemList.ts
@@ -0,0 +1,29 @@
+import {Component, EventEmitter, Input, Output, CORE_DIRECTIVES} from "angular2/angular2";
+
+@Component({
+    selector: "item-list",
+    template: `
+        <div style="border:2px solid blue;">
+            <h3>{{ title }} </h3>
+            <ul>
+                <li *ng-for="#p of items">
+                    <label>{{ p }}: <input type="checkbox" (click)="update($event, p)" [checked]="state==='y'"></label>
+                </li>
+            </ul>
+        </div>
+    `,
+    input: ['title', 'items', 'state'],
+    outputs: ['switchStatus: statusChange'],
+    directives: [CORE_DIRECTIVES]
+})
+export default class ItemList {
+    @Input() title;
+    @Input() items;
+    @Input() state;
+
+    private switchStatus:EventEmitter<any> = new EventEmitter<any>();
+
+    update(value, name) {
+        this.switchStatus.emit({value: event.target.checked, name: name});
+    }
+}
--- src/app/Page3.ts
+++ src/app/Page3.ts
@@ -1,25 +1,42 @@
 import {Component} from "angular2/angular2";
 
 import SearchBox from "./SearchBox";
+import ItemList from "./ItemList";
 
 @Component({
     selector: "page3",
     template: `
         <h1>Page 3</h1>
 
-        <div class="well">
-        Child updated filter: {{ filter }}
+        <div style="border:2px solid red">
+            <div class="well">
+                Search box value: {{ filter }}
+            </div>
+            <hr>
+            <search-box (filter-change)="onUpdate($event)"></search-box>
+            <item-list title="Active status" [items]="activeItems" (status-change)="aStatus($event)" state="y"></item-list>
+            <item-list title="Passive status" [items]="passiveItems" (status-change)="pStatus($event)" state="n"></item-list>
         </div>
-        <hr>
-        <search-box (filter-change)="onUpdate($event)"></search-box>
     `
-    ,directives:[SearchBox]
+    , directives: [SearchBox, ItemList]
 })
 export default class Page3 {
     public filter:string;
+    public activeItems:Array<string> = ["julia", "vadim"];
+    public passiveItems:Array<string> = ["apple", "orange"];
 
     onUpdate(value) {
-        debugger;
         this.filter = value;
     }
+
+    pStatus(val) {
+        this.passiveItems = this.passiveItems.filter((n)=> n !== val.name);
+        this.activeItems.push(val.name);
+    }
+
+    aStatus(val) {
+        this.activeItems = this.activeItems.filter((n)=> n !== val.name);
+        this.passiveItems.push(val.name);
+    }
+
 }
