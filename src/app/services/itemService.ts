aa74cd6a493e59a7922fd079ec8bac9376783c59
Julia.Passynkova <julia.passynkova@nurun.com>
2015-12-28 18:15:09 -0500

    list item

--- src/app/services/itemService.ts
+++ src/app/services/itemService.ts
@@ -1,14 +1,16 @@
+import Hero from "./../model/hero";
+
 export default class ItemService {
     public activeItems:Array<string> = ["bluberry", "banana"];
     public passiveItems:Array<string> = ["apple", "orange"];
 
     pStatus(val) {
-        this.passiveItems = this.passiveItems.filter((n)=> n !== val.name);
+        this.passiveItems.splice(this.passiveItems.indexOf(val.name), 1);
         this.activeItems.push(val.name);
     }
 
     aStatus(val) {
-        this.activeItems = this.activeItems.filter((n)=> n !== val.name);
+        this.activeItems.splice(this.activeItems.indexOf(val.name), 1);
         this.passiveItems.push(val.name);
     }
 }
