d8ca221ebce2d069bc7cc76b94dfc5aa977774f4
juliap <password>
2016-02-08 11:18:52 -0500

    child route

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,7 +14,7 @@ import Event from "./event/event";
 import PipeHost from "./pipe/pipe-host.component";
 import WebSocket from "./websocket/websocket";
 import ObservableList from "./list-observable/list";
-import NestedRoute from "./nested-route/nested-route.component";
+import {ChildRoute,ChildSummary} from "./child-route/child-route.component";
 
 @RouteConfig([
    {path: '/click-observable', component: ClickObservable, as: 'ClickObservable'},
@@ -28,14 +28,14 @@ import NestedRoute from "./nested-route/nested-route.component";
     {path: '/child-parent', component: ChildParent, as: 'ChildParent'},
     {path: '/event', component: Event, as: 'Event'},
     {path: '/pipe', component: PipeHost, as: 'PipeHost'},
-    {path: '/websocket', component: WebSocket, as: 'WebSocket'}
-   // {path: '/nested-route/...', component: NestedRoute, as: "NestedRoute", data:"'./NestedRoute','Summary'"}
+    {path: '/websocket', component: WebSocket, as: 'WebSocket'},
+    {path: '/child-route/...', component: ChildRoute, as: "ChildRoute"}
 ])
 @Component({
     selector: 'app',
     templateUrl: 'src/app/app.html',
     directives: [ROUTER_DIRECTIVES, Template, Form, List, ChildParent, TickObservable,
-        PanelList, FormObservable, ClickObservable, Event, PipeHost, WebSocket]
+        PanelList, FormObservable, ClickObservable, Event, PipeHost, WebSocket,ChildRoute]
 })
 export default class App {
     getRoutes():Array<Object> {
