f5f5732eae984b799cb0c8be0b278a2eb19b7756
Julia.Passynkova <julia.passynkova@nurun.com>
2016-02-11 02:02:47 -0500

    tslint

--- src/app/list-observable/item.service.ts
+++ src/app/list-observable/item.service.ts
@@ -5,43 +5,42 @@ import {BehaviorSubject} from 'rxjs/Rx';
 import {Observable} from 'rxjs/Observable';
 
 export class Item {
-    constructor(public name: string, public checked: boolean) {
-    }
+  constructor(public name:string, public checked:boolean) {
+  }
 }
 
 export interface State {
-    items: Array<Item>
+  items: Array<Item>;
 };
 
-const initItems: Array<Item> = [
-    new Item('blueberry', true),
-    new Item('banana', true),
-    new Item('apple', false),
-    new Item('orange', false)
+const initItems:Array<Item> = [
+  new Item('blueberry', true),
+  new Item('banana', true),
+  new Item('apple', false),
+  new Item('orange', false)
 ];
 
 @Injectable()
 export class ObservableItemService {
   public items$ = this.store$.map((s:State) => s.items);
-  public  dispatcher$: Subject<Item> = new Subject<Item>(null);
+  public  dispatcher$:Subject<Item> = new Subject<Item>(null);
 
-  private  store$: BehaviorSubject<State> = new BehaviorSubject<State>({items: initItems});
+  private  store$:BehaviorSubject<State> = new BehaviorSubject<State>({items: initItems});
   private reduce$ = new Subject<Item>(null);
 
   constructor() {
-      this.reduce$
-          .scan((state: State, {name, checked})=> {
-              let i = state.items.findIndex((x: Item) => x.name === name);
-              return {
-                  items:
-                      [...state.items.slice(0, i),
-                      new Item(checked ? name.toUpperCase() : name.toLowerCase(), checked),
-                      ...state.items.slice(i + 1)
-                      ]
-              };
-          }, {items: initItems})
-          .subscribe((s: State) => this.store$.next(s));
+    this.reduce$
+      .scan((state:State, {name, checked})=> {
+        let i = state.items.findIndex((x:Item) => x.name === name);
+        return {
+          items: [...state.items.slice(0, i),
+            new Item(checked ? name.toUpperCase() : name.toLowerCase(), checked),
+            ...state.items.slice(i + 1)
+          ]
+        };
+      }, {items: initItems})
+      .subscribe((s:State) => this.store$.next(s));
 
-      this.dispatcher$.subscribe(x => this.reduce$.next(x));
+    this.dispatcher$.subscribe(x => this.reduce$.next(x));
   }
 }
