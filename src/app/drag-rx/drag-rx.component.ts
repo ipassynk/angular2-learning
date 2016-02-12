f85111614df48ed4227c5562bc9f20c0982d7e9a
Julia.Passynkova <julia.passynkova@nurun.com>
2016-02-11 21:53:47 -0500

    change drag rx

--- src/app/drag-rx/drag-rx.component.ts
+++ src/app/drag-rx/drag-rx.component.ts
@@ -2,6 +2,7 @@ import {Component} from 'angular2/core';
 import {Subject} from "rxjs/Subject";
 import {ChangeDetectionStrategy} from "angular2/core";
 import ComponentDescriptionDecorator from "../decorator/conponent-description.decorator";
+import {Observable} from "rxjs/Observable";
 
 @ComponentDescriptionDecorator('Mouse RX events with angular atyle attr binding')
 @Component({
@@ -21,8 +22,7 @@ import ComponentDescriptionDecorator from "../decorator/conponent-description.de
   template: `
         <h3>Drag the cursor inside the red box and on course up the picture will follow</h3>
         <div class="box" (mousedown)="mousedown$.next($event)" (mouseup)="mouseup$.next($event)" (mousemove)="mousemove$.next($event)">
-            <img [style.left]="left$ | async" [style.top]="top$ | async"
-                  src="http://reactivex.io/assets/Rx_Icon.png">
+            <img [style.left]="left$ | async" [style.top]="top$ | async" src="http://reactivex.io/assets/Rx_Icon.png">
         </div>
     `
   , changeDetection: ChangeDetectionStrategy.OnPushObserve
@@ -31,18 +31,18 @@ export default class DragRx {
   mousedown$:Subject<MouseEvent> = new Subject<MouseEvent>();
   mouseup$:Subject<MouseEvent> = new Subject<MouseEvent>();
   mousemove$:Subject<MouseEvent> = new Subject<MouseEvent>();
-  top$:Subject<string> = new Subject<string>();
-  left$:Subject<string> = new Subject<string>();
+  top$:Observable<string>;
+  left$:Observable<string>;
 
   constructor() {
-    this.mousedown$
+    let imagepos$ = this.mousedown$
       .flatMap(x=>
         this.mousemove$
           .takeUntil(this.mouseup$)
       )
-      .subscribe((e:MouseEvent) => {
-        this.left$.next(e.clientX + 50 + "px");
-        this.top$.next(e.clientY + 50 + "px");
-      });
+      .map((e:MouseEvent) => [`${e.clientX + 50}px`, `${e.clientY + 50}px`]);
+
+    this.left$ = imagepos$.map(c=>c[0]);
+    this.top$ = imagepos$.map(c=>c[1]);
   }
 }
